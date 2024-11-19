c9990527853cdedd1a57dbc0c239229146cb1865
4xt_nqf <94098024+shubhi-tiwari@users.noreply.github.com>
2024-11-20 01:52:06 +0530

    Add files via upload

---
+++ client/src/post.js
@@ -0,0 +1,18 @@
+export default function Post() {
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://images.unsplash.com/photo-1720712738661-9c0dcb92f06d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        </div>
+        <div className="texts">
+          <h2>Scotland Road Trip Tips You Need to Know Before You Go</h2>
+          <p className="info">
+             <a className="author">Connor Anderson</a>
+             <time>2024-11-17 17:45</time>
+          </p>
+          <p className="summary">Scotland is a rugged and beautiful country that’s perfect for exploring on a road trip. The craggy coasts, placid lochs, and rolling hills make it a dream to discover. </p>
+        </div>
+        </div>
+
+    )
+}
